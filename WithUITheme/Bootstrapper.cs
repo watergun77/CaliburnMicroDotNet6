90fac225aca8b0941603892ee833e70b0d847d7c
Tan Jui Cheng <juicheng.tan@merry.com.sg>
2022-11-03 11:09:06 +0800

    Added initial WithUITheme project.

---
+++ WithUITheme/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using WithUITheme.ViewModels;
+
+namespace WithUITheme
+{
+    internal class Bootstrapper:BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
---
+++ WithUITheme/ViewModels/ShellViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace WithUITheme.ViewModels
+{
+    internal class ShellViewModel : Conductor<object>
+    {
+    }
+}
